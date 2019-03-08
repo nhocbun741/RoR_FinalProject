e6ec48025039efb055fb27de8c1d9ba6fdee4f16
Tho <nguyenhoangtrungtho9130@gmail.com>
2019-03-08 18:08:54 +0700

    get products by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController 
-    def index
+    def products
+        # @categories = Product.where(category: Category.find(params[:id]))
+        @products = Category.find(params[:id]).products.order(created_at: :desc).page params[:page]
         @categories = Category.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
-  resources :categories
+  resources :categories do
+    member do
+      get :products
+    end
+  end
+
 end
